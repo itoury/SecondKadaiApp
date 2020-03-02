fe367f6bdb1c573f4f620115f6d230e61fb3adf1
ryuuya.itou <itouruy@gmail.com>
2020-03-02 22:54:15 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 伊藤龍哉 on 2020/03/02.
+//  Copyright © 2020 ryuya.ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
